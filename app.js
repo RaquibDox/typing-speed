4996c23d0d65377c3755861cad41043e6927bfb9
RaquibDox <70raquib@gmail.com>
2022-10-07 13:54:56 +0530

    comments added and bug fixes

--- app.js
+++ app.js
@@ -14,13 +14,17 @@ let maxTime = 60,
   timerStarted = false,
   charIndex = (mistakes = size = lastInputIndex = words = 0);
 
+//this function randomly calls a paragraph and show it in the view
 function randomParagraph() {
   let randIndex = Math.floor(Math.random() * paragraphs.length);
   typingText.innerHTML = "";
   size = paragraphs[randIndex].split("").length;
   paragraphs[randIndex].split("").forEach((span) => {
-    let spanTag = `<span>${span}</span>`;
-    typingText.innerHTML += spanTag;
+    //removing everything except letters
+    if (span.toLowerCase() != span.toUpperCase() || span === " ") {
+      let spanTag = `<span>${span}</span>`;
+      typingText.innerHTML += spanTag;
+    }
   });
   typingText.querySelectorAll("span")[0].classList.add("active");
   scrollToCursor();
@@ -28,6 +32,7 @@ function randomParagraph() {
   typingText.addEventListener("click", () => inpField.focus());
 }
 
+//the main function calling all other functions(everything happens here)
 function initTyping() {
   //the timer will start
   if (!timerStarted) {
@@ -63,33 +68,28 @@ function initTyping() {
   characters.forEach((span) => span.classList.remove("active"));
   characters[charIndex].classList.add("active");
   scrollToCursor();
-
-  let wpm = Math.round(
-    ((charIndex - mistakes) / 5 / (maxTime - timeLeft)) * 60
-  );
-  wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
-
-  mistakeTag.innerText = mistakes;
-  cpmTag.innerText = charIndex - mistakes;
-  wpmTag.innerText = wpm;
 }
 
+//for typing sound
 function typing() {
   audio.play();
 }
 
+//starts the timer and also switch it offs automatically after time runs out
 function startTimer() {
   interval = setInterval(() => {
     if (timeLeft <= 1) {
-      offTimer = false;
       clearInterval(interval);
       inpField.value = "";
+      updateTags();
     }
     timeLeft--;
     timerTag.innerText = timeLeft;
+    updateTags();
   }, 1000);
 }
 
+//resets everything and brings a new paragraph
 function resetGame() {
   randomParagraph();
   timeLeft = maxTime;
@@ -103,11 +103,25 @@ function resetGame() {
   scrollToCursor();
 }
 
+//used for updating the mistakes, wpm, cpm
+function updateTags() {
+  let wpm = Math.round(
+    ((charIndex - mistakes) / 5 / (maxTime - timeLeft)) * 60
+  );
+  wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+
+  mistakeTag.innerText = mistakes;
+  cpmTag.innerText = charIndex - mistakes;
+  wpmTag.innerText = wpm;
+}
+
+//scrolls the lines according to the text cursor
 function scrollToCursor() {
   let activeCursor = document.querySelector(".active");
   activeCursor.scrollIntoView(true);
 }
 
+//activates when you type something
 inpField.addEventListener("input", () => {
   if (timeLeft > 0 && charIndex < size - 1) {
     typing();
@@ -117,9 +131,11 @@ inpField.addEventListener("input", () => {
   }
 });
 
+//resets when you reload the page
 window.addEventListener("DOMContentLoaded", () => {
   inpField.value = "";
   randomParagraph();
 });
 
+//resets when you press the button
 tryAgainBtn.addEventListener("click", resetGame);
